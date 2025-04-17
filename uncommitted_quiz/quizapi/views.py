f9b7550fdafb3862943f008bd3c5c1b5d89a46c8
Mark Butler <markbutlermb1991@gmail.com>
2025-04-17 15:14:01 +0100

    set up ai powered question generator

--- uncommitted_quiz/quizapi/views.py
+++ uncommitted_quiz/quizapi/views.py
@@ -3,23 +3,28 @@ from django.http import JsonResponse
 from .utils import gen_quiz_question
 
 def get_quiz_questions(request):
-    if quiz_questions not in request.session:
+    if 'quiz_questions' not in request.session:
         request.session['quiz_questions'] = []
         request.session['question_count'] = 0
-        if request.session['question_count'] >= 5:
-            request.session.flush()
-            return JsonResponse({
-                "complete": True,
-                "score": 100%
-                "message": "You scored 100% because being willing to try makes you a coder"
-            })
-        
+
+    # Check if we have reached 5 questions
+    if request.session['question_count'] >= 5:
+        request.session.flush()  # Clear session and reset
+        return JsonResponse({
+            "complete": True,
+            "score": "100%",
+            "message": "Well done, you win! Keep up the great work!"
+        })
+
+    # Get a new question and add it to the session
     question = gen_quiz_question()
     request.session['quiz_questions'].append(question)
     request.session['question_count'] += 1
+
     return JsonResponse({
         "complete": False,
         "question": question['question'],
+        "answers": question['answers'], 
         "current": request.session['question_count'],
         "total": 5,
     })
