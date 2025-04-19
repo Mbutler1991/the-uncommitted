0be8d8fbde5109cd9795919afee04481b5ef075b
OJarvey <165787104+OJarvey@users.noreply.github.com>
2025-04-19 15:30:12 +0100

    Update settings

--- project/uncommitted_quiz/settings.py
+++ project/uncommitted_quiz/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'project.quizapi',
+    'quizapi',
     'openai',
     'home',
     # 'rest_framework',
