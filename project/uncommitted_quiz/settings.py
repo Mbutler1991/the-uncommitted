c8130b8a89989af3daa8cf649e96456fd9255b47
Mark Butler <markbutlermb1991@gmail.com>
2025-04-19 19:07:19 +0100

    Update settings.py

--- project/uncommitted_quiz/settings.py
+++ project/uncommitted_quiz/settings.py
@@ -37,7 +37,18 @@ ALLOWED_HOSTS = [
 ]
 
 
-# Application definition
+CSRF_TRUSTED_ORIGINS = [
+    'https://uncommitted-quiz-1141a858de24.herokuapp.com',
+    'http://localhost:8000',  # For local development
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'https://uncommitted-quiz-1141a858de24.herokuapp.com',
+    'http://localhost:8000',
+]
+
+CSRF_COOKIE_SECURE = True  
+SESSION_COOKIE_SECURE = True 
 
 INSTALLED_APPS = [
     "django.contrib.admin",
