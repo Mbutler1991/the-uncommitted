288c29028bec48beaf6a2d358ee18a0053d47aed
Mark Butler <markbutlermb1991@gmail.com>
2025-04-19 15:19:13 +0100

    Update settings.py

--- project/uncommitted_quiz/settings.py
+++ project/uncommitted_quiz/settings.py
@@ -44,10 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quizapi',
-    'openai',
     'home',
-    # 'rest_framework',
+    'quizapi',
 ]
 
 MIDDLEWARE = [
