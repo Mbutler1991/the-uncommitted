e3a4ca37c7ec5a5be66d6d2573b64a5ddff74761
Mark Butler <markbutlermb1991@gmail.com>
2025-04-19 14:37:48 +0100

    grasping at straws

--- project/uncommitted_quiz/settings.py
+++ project/uncommitted_quiz/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quizapi',
+    'project/quizapi',
     'openai',
     'home',
     # 'rest_framework',
