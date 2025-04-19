99adda1e9ec77808df91a133810ca9c0e131d1c3
Mark Butler <markbutlermb1991@gmail.com>
2025-04-19 15:29:59 +0100

    Update manage.py

--- project/manage.py
+++ project/manage.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
+    # Add project directory to Python path
+    project_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(project_path)
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uncommitted_quiz.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -16,7 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
