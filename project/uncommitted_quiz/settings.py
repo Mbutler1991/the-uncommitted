4687498545406b60f18d72429f4ce70a223a60ae
Mark Butler <markbutlermb1991@gmail.com>
2025-04-19 15:16:05 +0100

    Update settings.py

--- project/uncommitted_quiz/settings.py
+++ project/uncommitted_quiz/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'project.quizapi',
+    'quizapi',
     'openai',
     'home',
     # 'rest_framework',
